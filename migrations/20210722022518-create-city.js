5dd4ccc8f07d5fb7f83bf2f54c19367f23996445
forrestpark <pjwoo3@gmail.com>
2021-07-22 11:25:43 +0900

    added a city model

---
+++ migrations/20210722022518-create-city.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sido: {
+        type: Sequelize.STRING
+      },
+      sgg: {
+        type: Sequelize.STRING
+      },
+      population: {
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Cities');
+  }
+};
