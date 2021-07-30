fded6289ad52904bb92c4d9b6de36744367271c4
forrestpark <pjwoo3@gmail.com>
2021-07-30 17:42:08 +0900

    edited environment credentials for deployment

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const env = process.env;
+
+const development = {
+    "username": env.POSTGRES_USERNAME,
+    "password": env.POSTGRES_PASSWORD,
+    "database": env.POSTGRES_DATABASE,
+    "host": "localhost",
+    "dialect": "postgres"
+}
+
+const test = {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+
+const production = {
+  "use_env_variable": "DATABASE_URL",
+  "dialect": "postgres",
+  "dialectOptions": {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false,
+    }
+  }
+}
+
+module.exports = {development, test, production}
