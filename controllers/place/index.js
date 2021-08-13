141c84b3eb227ff540ae30706decdf576abfb221
forrestpark <pjwoo3@gmail.com>
2021-08-13 18:06:45 +0900

    added several places endpoints

--- controllers/place/index.js
+++ controllers/place/index.js
@@ -3,8 +3,10 @@ const { Router } = require('express');
 const router = Router();
 const ctrl = require('./place.ctrl');
 
-// router.get('/findAll', ctrl.get_all_cities);
-// router.post('/findOne', ctrl.get_one_city);
+router.get('/findAll', ctrl.get_all_places);
+router.post('/findBySidoSgg', ctrl.get_places_by_sido_sgg);
+router.post('/findByTheme', ctrl.get_places_by_theme);
+router.post('/findBySidoSggAndTheme', ctrl.sort_places_by_theme_for_certain_city);
 // router.post('/create', ctrl.create_city);
 // router.put('/update', ctrl.update_city);
 // router.delete('/delete', ctrl.delete_city);
--- controllers/place/place.ctrl.js
+++ controllers/place/place.ctrl.js
@@ -10,13 +10,77 @@ exports.get_all_places = async (req, res) => {
     }
 }
 
-// exports.name = async (req, res) => {
-//     try {
-//     } catch (err) {
-//         console.log(err)
-//         return res.status(500).json(err)
-//     }
-// }
+exports.get_places_by_sido_sgg = async (req, res) => {
+    const {sido_sgg} = req.body
+    try {
+        const places = await Place.findAll({
+            where : {
+                sido_sgg : sido_sgg
+            }
+        })
+        return res.json(places)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+}
+
+exports.get_places_by_theme = async (req, res) => {
+    const {theme} = req.body
+    try {
+        const places = await Place.findAll({
+            where : {
+                theme : theme
+            }
+        })
+        return res.json(places)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+}
+
+exports.sort_places_by_theme_for_certain_city = async (req, res) => {
+    var mountain_list = new Array()
+    var valley_list = new Array()
+    var beach_list = new Array()
+    var river_list = new Array()
+
+    const {sido_sgg} = req.body
+    try {
+        // all places in given city
+        const places = await Place.findAll({
+            where : {
+                sido_sgg : sido_sgg
+            },
+            raw: true
+        })
+
+        for (var i = 0; i < places.length; i++) {
+            var theme = places[i].theme
+            switch (theme) {
+                case "산":
+                    mountain_list.push(places[i])
+                case "계곡":
+                    valley_list.push(places[i])
+                case "해변":
+                    beach_list.push(places[i])
+                case "강":
+                    river_list.push(places[i])
+            }
+        }
+
+        const allCombined = {"mountains" : mountain_list, "valleys" : valley_list, "beaches" : beach_list, "rivers" : river_list}
+
+        return res.json(allCombined)
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+}
+
+
 
 // exports.name = async (req, res) => {
 //     try {
