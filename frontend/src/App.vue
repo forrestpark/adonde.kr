0d141b2aa7900bf4859ac4292dd87d242554dd8a
jisunchung <65282581+jisunchung@users.noreply.github.com>
2021-08-18 13:51:12 +0900

    added overlay

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -67,7 +67,7 @@
       <v-divider />
 
       <v-list dense>
-        <!-- <v-list-item
+        <v-list-item
           v-for="item in items" 
           :disabled="item.disabled"
           :key="item.title"
@@ -82,35 +82,10 @@
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
-        </v-list-item> -->
-        <v-list-item
-          link
-          to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>
-              Home
-            </v-list-item-title>
-          </v-list-item-content>
         </v-list-item>
-        <v-list-item
-          @click="clickLoginBtn">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title
-              v-if="user.email == undefined">
-              Login
-            </v-list-item-title>
-            <v-list-item-title
-              v-else>
-              Loout
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+       
+        
+        
       </v-list>
     </v-navigation-drawer>
 
@@ -285,15 +260,6 @@ export default {
       this.overlay = false
       //this.$router.push({path:'/'})
      
-    },
-    clickLoginBtn(){
-      if(this.user.email == undefined){
-        this.overlay = true
-        this.drawer = false
-      }else{
-        this.kakaoLogout()
-        this.drawer = false
-      }
     }
   }
 };
