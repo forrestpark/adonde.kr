7e67e300772df6503feca75b27f261e74ead4af2
forrestpark <pjwoo3@gmail.com>
2021-08-26 20:12:58 +0900

    changed .env import setting

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const session = require('express-session')
 var passport = require('passport')
 var SequelizeStore = require("connect-session-sequelize")(session.Store);
 const url = require('url')
+const dotenv = require('dotenv').config() 
 const db_url = process.env.PRODUCTION_BACKEND_URL
 const client_url = process.env.PRODUCTION_FRONTEND_URL
 // const LocalStrategy = require('passport-local').Strategy;
 
-const dotenv = require('dotenv').config() 
+
 
 class App {
 
