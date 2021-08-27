30dd6e759a9197d781b9389f3b5be31384fa5654
jisunchung <65282581+jisunchung@users.noreply.github.com>
2021-08-27 17:19:52 +0900

    troubleshooting map

--- frontend/src/components/ShowMap.vue
+++ frontend/src/components/ShowMap.vue
@@ -45,6 +45,7 @@ export default {
         ]),
         initMap() { 
             document.cookie = "crossCookie=bar; SameSite=None; Secure";
+            console.log('initmap')
             var container = document.getElementById('map'); 
             var options = { 
                 center: new kakao.maps.LatLng(33.450701, 126.570667), 
@@ -53,6 +54,8 @@ export default {
             //지도를 생성해준다
             var map = new kakao.maps.Map(container, options); 
             this.updateMap(map)
+
+
 
             // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
             // var bounds = new kakao.maps.LatLngBounds();   
